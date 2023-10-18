d3f8c44911a86df6598b4c26e60a9196b66bc8ad
Hey-Claudia <chclaudiahesse@gmail.com>
2023-10-18 14:35:11 +0200

    Fixed HTML & JS for Challenge 1

---
+++ configuration.js
@@ -0,0 +1,2 @@
+export const company = 'ACME Inc.'         /*changed "" to ' */
+export const year = 2022                  /*added export to const so it can be exported*/
---
+++ scripts.js
@@ -0,0 +1,7 @@
+import { company, year } from './configuration.js'  //added curly brackets to syntax and merged import into one line instead of 2 seprate imports
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const message = '© ' + company + ' (' + year + ')'
+    document.querySelector('footer').innerText = message
+});
